d4585fde3303ccc64e9d937441ff8b8dea224272
adrianBrands <adrianbrandshaug@hotmail.com>
2022-11-30 00:15:54 +0100

    added redirection if user is not logged in

--- src/js/profile/loggedIn.js
+++ src/js/profile/loggedIn.js
@@ -1,17 +1,23 @@
 const profileDropDown = document.querySelector("#profile-dropDown");
-const signInButton = document.getElementById('signIn');
-const signUpButton = document.getElementById("signUp")
+const signInButton = document.getElementById("signInButton");
+const signUpButton = document.getElementById("signUpButton");
+const path = location.pathname;
 
-export function isLoggedIn () {
-  const key = localStorage.getItem("token")
+export function isLoggedIn() {
+  const key = localStorage.getItem("token");
   if (!key) {
     profileDropDown.style.display = "none";
+    //window.location.replace("/index.html");
     
-  } if (key) {
+  }
+  if (path !== "/index.html" && !key) {
+    window.location.replace("/index.html");
+  }
+  if (key) {
     signInButton.style.display = "none";
     signUpButton.style.display = "none";
+    profileDropDown.style.display = "block";
   } else {
     return false;
   }
-
 }
