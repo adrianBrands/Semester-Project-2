d7e34d14297c34c6e37ffd924c6edb60744d6e37
adrianBrands <adrianbrandshaug@hotmail.com>
2022-12-09 17:55:04 +0100

    adding user content to page

--- src/js/listings/listings_id.js
+++ src/js/listings/listings_id.js
@@ -19,11 +19,51 @@ function listingImage(listingData, container){
   });
 }
 
+function listingTitle(listingData, container){
+  //const listing = listingData.map((title) => title);
+  //listing.forEach(function(element) {
+    const title = document.createElement("h1");
+    title.innerText = listingData.title;
+    container.prepend(title);
+ // })
+
+}
+
+function listingContent(listingData, container){
+  const listingOwnerContent = document.createElement("div");
+  listingOwnerContent.classList.add("d-flex", "justify-content-between", "listingOwner");
+
+  const listingOwnerName = document.createElement("h2");
+  listingOwnerName.innerText = listingData.seller.name;
+
+  const listingOwnerImage = document.createElement("img");
+  listingOwnerImage.className = "rounded-circle";
+  listingOwnerImage.setAttribute("src", listingData.seller.avatar);
+  listingOwnerImage.setAttribute("width", "45");
+  listingOwnerImage.setAttribute("height", "45");
+  listingOwnerImage.setAttribute("alt", "user avatar image");
+
+  listingOwnerContent.append(listingOwnerName, listingOwnerImage);
+
+  //const after = document.querySelector(".col-lg-8");
+
+  //listingOwnerContent.after(after)
+  
+  container.prepend(listingOwnerContent)
+
+
+}
+
 
 async function testListing() {
   const listing = await listingIdAPI();
+  console.log(listing)
   const container = document.querySelector(".carousel-inner");
-  listingImage(listing, container)
+  const titleContainer = document.querySelector(".col-lg-8");
+  const contentContainer = document.querySelector(".listingOwner");
+  listingImage(listing, container);
+  listingTitle(listing, titleContainer);
+  listingContent(listing, contentContainer);
 }
 
 testListing()
