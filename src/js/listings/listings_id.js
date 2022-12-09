9b7d68b3b374e7998d52ce02b38fb683405a48fb
adrianBrands <adrianbrandshaug@hotmail.com>
2022-12-09 13:31:30 +0100

    modified listing_id.js

--- src/js/listings/listings_id.js
+++ src/js/listings/listings_id.js
@@ -1,69 +1,29 @@
 import { listingIdAPI } from "../api/listings/listing_id.js";
 
 
-function listingImage(listingData){
-    
-  
-  
-  
-  
-    
-    
-
-    
-    
-  
-  
-  
-
- 
-  
-}
-
-
-async function testListing() {
-  const listings = await listingIdAPI();
-  const container = document.querySelector(".carousel-inner");
-  //container.append(listingImage(listings))
-  //listingId(listings, container)
-  //listingId(listings)
-  
- 
-  
-    
-  
-    const test = listings.media.map((media) => media);
-    
-    
-  
-  
-    test.forEach(function(element, index) {
-      
-      
+function listingImage(listingData, container){
+  const listing = listingData.media.map((media) => media);
+  listing.forEach(function(element, index) {
     const carouselItem = document.createElement("div");
     if(index === 0){
       carouselItem.classList.add("carousel-item", "active")
     } else if (index !== 0 ){
       carouselItem.classList.add("carousel-item")
     }
+    
     const carouselImage = document.createElement("img");
     carouselImage.setAttribute("src", element);
-    //console.log(carouselImage)
     carouselImage.classList.add("d-block", "w-100");
     carouselItem.append(carouselImage);
     container.append(carouselItem)
-    /*container.innerHTML += `<div class="carousel-item">
-    <img src=${hello} class="d-block w-100" alt="...">
-  </div>`*/
-      
-    });
-    
+  });
+}
 
-    
 
-    
-  
-  
+async function testListing() {
+  const listing = await listingIdAPI();
+  const container = document.querySelector(".carousel-inner");
+  listingImage(listing, container)
 }
 
 testListing()
