9ee7257412847db1f63db08fe4ee26503a92ba00
adrianBrands <adrianbrandshaug@hotmail.com>
2022-12-10 01:09:49 +0100

    modified: listing.html, src/js/listings/listings_id.js

--- src/js/listings/listings_id.js
+++ src/js/listings/listings_id.js
@@ -1,10 +1,13 @@
 import { listingIdAPI } from "../api/listings/listing_id.js";
+import { userInfo } from "../localStorage/storage.js"
 
 
 function listingImage(listingData, container){
   const listing = listingData.media.map((media) => media);
+  
   listing.forEach(function(element, index) {
     const carouselItem = document.createElement("div");
+    
     if(index === 0){
       carouselItem.classList.add("carousel-item", "active")
     } else if (index !== 0 ){
@@ -14,18 +17,17 @@ function listingImage(listingData, container){
     const carouselImage = document.createElement("img");
     carouselImage.setAttribute("src", element);
     carouselImage.classList.add("d-block", "w-100");
+    
     carouselItem.append(carouselImage);
     container.append(carouselItem)
   });
 }
 
 function listingTitle(listingData, container){
-  //const listing = listingData.map((title) => title);
-  //listing.forEach(function(element) {
-    const title = document.createElement("h1");
-    title.innerText = listingData.title;
-    container.prepend(title);
- // })
+  const title = document.createElement("h1");
+  title.innerText = listingData.title;
+  container.prepend(title);
+ 
 
 }
 
@@ -39,17 +41,71 @@ function listingContent(listingData, container){
   const listingOwnerImage = document.createElement("img");
   listingOwnerImage.className = "rounded-circle";
   listingOwnerImage.setAttribute("src", listingData.seller.avatar);
-  listingOwnerImage.setAttribute("width", "45");
-  listingOwnerImage.setAttribute("height", "45");
+  listingOwnerImage.setAttribute("width", "50");
+  listingOwnerImage.setAttribute("height", "50");
   listingOwnerImage.setAttribute("alt", "user avatar image");
 
   listingOwnerContent.append(listingOwnerName, listingOwnerImage);
-
-  //const after = document.querySelector(".col-lg-8");
-
-  //listingOwnerContent.after(after)
   
-  container.prepend(listingOwnerContent)
+
+  const listingContent = document.createElement("div");
+  listingContent.className = "mt-3";
+
+  const listingDescription = document.createElement("p");
+  listingDescription.innerText = listingData.description;
+
+  const bids = listingData.bids;
+  function getHighestBid(bid){
+    return bid.amount - 1;
+  }
+  const highestBid = bids.findLast(getHighestBid)
+
+  /*const listingss = listing.bids;
+  function getHighestBid(bid){
+    return bid.amount - 1;
+  }
+  const highestBid = listingss.findLast(getHighestBid)
+
+  console.log(highestBid.amount)*/
+  
+  const listingBid = document.createElement("p");
+  listingBid.classList.add("mb-1", "mt-5");
+  listingBid.innerText = `Current bid: $${highestBid.amount}`
+
+  const totalBids = document.createElement("p");
+  totalBids.className = "border-bottom";
+  totalBids.innerText = `${listingData._count.bids} bids`;
+
+  const bidForm = document.createElement("form");
+  bidForm.className = "mt-5";
+  
+  const label = document.createElement("label");
+  label.className = "form-label";
+  label.innerText = `Your Current Credit: $${userInfo().userCredits}`;
+  
+  const inputGroup = document.createElement("div");
+  inputGroup.className = "input-group";
+  const inputGroupText = document.createElement("span");
+  inputGroupText.className = "input-group-text";
+  inputGroupText.innerText = "$";
+  
+  const input = document.createElement("input");
+  input.className = "form-control";
+  input.setAttribute("type", "text");
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-outline-primary");
+  button.setAttribute("type", "button");
+  button.id = "button-addon2";
+  button.innerText = "button";
+
+  inputGroup.append(inputGroupText, input, button);
+
+  bidForm.append(label, inputGroup)
+
+  listingContent.append(listingDescription, listingBid, totalBids, bidForm)
+
+  container.append(listingOwnerContent, listingContent);
+
 
 
 }
@@ -57,7 +113,10 @@ function listingContent(listingData, container){
 
 async function testListing() {
   const listing = await listingIdAPI();
-  console.log(listing)
+  console.log(listing);
+  
+  
+  
   const container = document.querySelector(".carousel-inner");
   const titleContainer = document.querySelector(".col-lg-8");
   const contentContainer = document.querySelector(".listingOwner");
