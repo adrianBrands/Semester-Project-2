8f5f39cf9f4635ac33ff985d112fec0818764c7f
adrianBrands <adrianbrandshaug@hotmail.com>
2022-12-18 22:41:37 +0100

    modified code

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,17 @@
 import { signUpFormListener } from "./listeners/auth/signUp.js";
 import { signInFormListener } from "./listeners/auth/signIn.js";
-
 import { signOutUser } from "./listeners/auth/signOut.js";
 import { isLoggedIn } from "./profile/loggedIn.js";
-
 import { changeAvatarImage } from "./listeners/Profile/avatar.js";
 import { userProfile } from "./profile/profile.js";
 import { userProfileImage } from "./profile/profile.js";
-
 import { bidListener } from "./listeners/Profile/credits.js";
-
 import { displayListing } from "./listings/listings_id.js";
 import { displayListings } from "./listings/listings.js";
 import { testListings } from "./profile/profileListings.js";
 import { createFormListener } from "./listings/create.js";
 
 if (location.pathname === "/index.html") {
-  console.log(true);
   signUpFormListener();
   signInFormListener();
   displayListings();
